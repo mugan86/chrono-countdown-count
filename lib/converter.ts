0c9bd06076e11102e73d1e399e2b927e1e87f2e7
Anartz Mugika <mugan86@gmail.com>
2018-12-26 09:56:33 +0100

    Adapt correctly and resolve issues

--- lib/converter.ts
+++ lib/converter.ts
@@ -8,21 +8,22 @@ import { Timer } from "./constants";
  * @param clockFormat Determine if return format take clock or d/h/m/s
  */
 export function secondsInTimeFormat(timeInSeconds: number, type: number, limit: number = 0, clockFormat: boolean = false) {
-    if (timeInSeconds < 0 || timeInSeconds === undefined || timeInSeconds === null || type === 1 && timeInSeconds === limit || type === 2 && timeInSeconds === 0) {
+    if (checkTimerFinish(timeInSeconds, type, limit)) {
         return 'FINISH';
     }
     const days = Math.floor(timeInSeconds / (Timer.ONE_DAY_SECONDS));
     const hours = Math.floor((timeInSeconds % (Timer.ONE_DAY_SECONDS))
-                    / (Timer.ONE_HOUR_SECONDS));
+        / (Timer.ONE_HOUR_SECONDS));
     const minutes = Math.floor((timeInSeconds % (Timer.ONE_HOUR_SECONDS)) / (Timer.ONE_MIN_SECONDS));
     const seconds = Math.floor((timeInSeconds % (Timer.ONE_MIN_SECONDS)));
     if (clockFormat) {
         if (days === 0) {
             return numberFormatClock(hours)
-                    + ':' + numberFormatClock(minutes)
-                    + ':' + numberFormatClock(seconds);
+                + ':' + numberFormatClock(minutes)
+                + ':' + numberFormatClock(seconds);
         }
-        return days+'d - ' + hours + ':' + minutes + ':' + seconds;
+        return days + 'd - ' + numberFormatClock(hours) 
+                + ':' + numberFormatClock(minutes) + ':' + numberFormatClock(seconds);
     }
     return days + 'd ' + hours + 'h '
         + minutes + 'm ' + seconds + 's';
@@ -33,5 +34,19 @@ export function secondsInTimeFormat(timeInSeconds: number, type: number, limit: 
  * @param n Input number to return with two digits
  */
 function numberFormatClock(n: number) {
-    return (n < 10 ) ? '0'.concat(n.toString()) : n;
+    return (n < 10) ? '0'.concat(n.toString()) : n;
+}
+
+/**
+ * Check if counter (count or discount) arrive to finish
+ * @param timeInSeconds time in number format to specify total seconds in this moment
+ * @param type counter type - 1: Counter Up / 2: Counter Down
+ * @param limit Finish number
+ */
+function checkTimerFinish(timeInSeconds: number, type: number, limit: number) {
+    if (timeInSeconds < 0 || timeInSeconds === undefined || timeInSeconds === null
+        || type === 1 && timeInSeconds === limit || type === 2 && timeInSeconds === 0) {
+        return true;
+    }
+    return false;
 }
